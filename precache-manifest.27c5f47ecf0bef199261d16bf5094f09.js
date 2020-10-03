66c013345b3ea64b808ce03f8f2ab2d7500e1de6
Bakhrom <baxromsadullayev@mail.ru>
2020-10-04 02:24:23 +0500

    Updates

---
+++ precache-manifest.27c5f47ecf0bef199261d16bf5094f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23bce67bd833b2c34c1c55f540e5a4a",
+    "url": "/BakhromjonSadullaev/react/index.html"
+  },
+  {
+    "revision": "6590b4896d251fb94e87",
+    "url": "/BakhromjonSadullaev/react/static/css/main.b8793f8c.chunk.css"
+  },
+  {
+    "revision": "64e30766d470bd3d59ab",
+    "url": "/BakhromjonSadullaev/react/static/js/2.b01319ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BakhromjonSadullaev/react/static/js/2.b01319ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6590b4896d251fb94e87",
+    "url": "/BakhromjonSadullaev/react/static/js/main.5c0c1bdc.chunk.js"
+  },
+  {
+    "revision": "ba0cebe8c9a294c6a499",
+    "url": "/BakhromjonSadullaev/react/static/js/runtime-main.94da62cb.js"
+  }
+]);
